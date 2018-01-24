1c62e90e6593f58dff5a9377fb935ebfc071cefb
Nisali Kularatne <nisalikularatne@yahoo.com>
2018-01-24 14:56:48 +0800

    WordModel implemented. A Word class to handle Words in the WordNet database and a Definition class to get the definitions of respective words.

---
+++ engine/domain/WordModel.py
@@ -0,0 +1,31 @@
+from nltk.corpus import wordnet as wn
+import random
+class Word:
+    def __init__(self, word, difficulty=0):
+        self.word = word
+        self.difficulty = difficulty
+        self.definitions = [Definition(sense) for sense in wn.synsets(self.word)]
+        self.definition = random.choice(self.definitions)
+    def __str__(self):
+        return "The Word is: {}".format(self.word)
+
+
+
+class Definition:
+    def __init__(self, sense):
+        self.definition = sense.definition()
+        self.sense = sense
+        self.pos = sense.pos() #pos : parts of speech
+
+
+
+    def __repr__(self):
+        return "{}".format(self.definition)
+
+
+
+if __name__ == "__main__":
+
+    w = Word("world")
+    print(w.definition)
+    print(w)
