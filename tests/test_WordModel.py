5af7977f837a00b51950fe8c3957d715c9130a69
Nisali Kularatne <nisalikularatne@yahoo.com>
2018-01-26 21:49:03 +0400

    check travis pass

--- tests/test_WordModel.py
+++ tests/test_WordModel.py
@@ -3,4 +3,8 @@ from hamcrest import *
 from engine.domain.WordModel import Word
 class TestWordModel(unittest.TestCase):
     def test_definitions(self):
-       print('hi')
+        oldword=Word("actor").definition
+        newword=str(oldword)
+        print(newword)
+        assert_that(newword,any_of('a person who acts and gets things done', 'a theatrical performer'))
+
