0e9b0073ba09a9005dbf41af89d929534d833b48
Nisali Kularatne <nisalikularatne@yahoo.com>
2018-01-28 17:18:25 +0400

    path name changed to travis 3

--- engine/domain/distractor_selection.py
+++ engine/domain/distractor_selection.py
@@ -1,12 +1,18 @@
 import pickle
 from collections import OrderedDict
 import os
+import sys
+import pkg_resources
 from nltk.corpus import wordnet as wn
 from engine.domain.WordModel import Word
 script_path = os.path.dirname(os.path.realpath(__file__))
+ROOT_DIR = os.path.dirname(sys.modules['__main__'].__file__)
+PROJECT_ROOT = os.path.abspath(os.path.dirname())
+DEFINITIONS_ROOT = os.path.join(PROJECT_ROOT, 'resources', 'wordlist_byalphabets')
+CONFIG_PATH = pkg_resources.resource_filename('resources', 'wordlist_byalphabets')
 path = '/home/travis/build/nisalikularatne/SmartVocab/resources/wordlist_byalphabets'
 def read_file_ordered_by_frequency():
-    with open(path,'r') as f:
+    with open(DEFINITIONS_ROOT,'r') as f:
         for line in f.readlines():
             l = line.strip().split()
             freq = l[1]
@@ -16,3 +22,4 @@ def read_file_ordered_by_frequency():
 if __name__ == "__main__":
     print(read_file_ordered_by_frequency())
     print(next(read_file_ordered_by_frequency()))
+    print('hi')
