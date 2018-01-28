594b48938857e7456ec55ba60af7cc5fe8b0381f
Nisali Kularatne <nisalikularatne@yahoo.com>
2018-01-28 22:58:56 +0400

    test for read_file_by_alphabet added

--- engine/domain/distractor_selection.py
+++ engine/domain/distractor_selection.py
@@ -7,9 +7,20 @@ import inspect
 from nltk.corpus import wordnet as wn
 from engine.domain.WordModel import Word
 script_path = os.path.dirname(os.path.realpath(__file__))
-cmd_subfolder = os.path.realpath(os.path.abspath(os.path.join(os.path.split(inspect.getfile( inspect.currentframe() ))[0],"../../resources/wordlist_byalphabets")))
+frequency_list = os.path.realpath(os.path.abspath(os.path.join(os.path.split(inspect.getfile( inspect.currentframe() ))[0],"../../resources/wordlist_byfrequency")))
+alphabet_list = os.path.realpath(os.path.abspath(os.path.join(os.path.split(inspect.getfile( inspect.currentframe() ))[0],"../../resources/wordlist_byalphabets")))
+
 def read_file_ordered_by_frequency():
-    with open(cmd_subfolder,'r') as f:
+    with open(frequency_list,'r') as f:
+        for line in f.readlines():
+            l = line.strip().split()
+            freq = l[1]
+            word = l[2]
+            pos = l[3]
+            yield freq, word, pos
+
+def read_file_ordered_by_alphabet():
+     with open(alphabet_list, 'r') as f:
         for line in f.readlines():
             l = line.strip().split()
             freq = l[1]
@@ -19,4 +30,5 @@ def read_file_ordered_by_frequency():
 if __name__ == "__main__":
     print(read_file_ordered_by_frequency())
     print(next(read_file_ordered_by_frequency()))
+    print(next(read_file_ordered_by_alphabet()))
     print('hi')
--- tests/test_distractor_selection.py
+++ tests/test_distractor_selection.py
@@ -5,7 +5,14 @@ path = 'C:\\Users\\Nisali Kularatne\\Documents\\f\\resources\\wordlist_byalphabe
 class TestDistractorSelection(unittest.TestCase):
 
     def test_read_file_by_frequency(self):
+        expected_res=('6187267', 'the', 'det')
+        res=next(read_file_ordered_by_frequency())
+        self.assertEqual(expected_res,res)
+
+    def test_read_file_by_alphabet(self):
         expected_res=('2186369', 'a', 'det')
         res=next(read_file_ordered_by_frequency())
         self.assertEqual(expected_res,res)
+
+
 
