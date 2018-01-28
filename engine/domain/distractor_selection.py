a0ce0119ebbeb641a4184846d03fc4cd0b59bbb7
Nisali Kularatne <nisalikularatne@yahoo.com>
2018-01-28 12:40:29 +0400

    distractor selection class added with frequencty word resources

---
+++ engine/domain/distractor_selection.py
@@ -0,0 +1,18 @@
+import pickle
+from collections import OrderedDict
+import os
+from nltk.corpus import wordnet as wn
+from engine.domain.WordModel import Word
+script_path = os.path.dirname(os.path.realpath(__file__))
+path = 'C:\\Users\\Nisali Kularatne\\Documents\\final year project\\resources\\wordlist_byalphabets'
+def read_file_ordered_by_frequency():
+    with open(path,'r') as f:
+        for line in f.readlines():
+            l = line.strip().split()
+            freq = l[1]
+            word = l[2]
+            pos = l[3]
+            yield freq, word, pos
+if __name__ == "__main__":
+    print(read_file_ordered_by_frequency())
+    print(next(read_file_ordered_by_frequency()))
---
+++ tests/test_distractor_selection.py
@@ -0,0 +1,8 @@
+import unittest
+from hamcrest import *
+
+class TestDistractorSelection(unittest.TestCase):
+    def test_read_file_by_frequency(self):
+        
+        assert_that(newword,any_of('a person who acts and gets things done', 'a theatrical performer'))
+
