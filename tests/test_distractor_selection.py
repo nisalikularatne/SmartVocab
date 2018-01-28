ba7040dddd147a9cd1c557a425c5e1913275011c
Nisali Kularatne <nisalikularatne@yahoo.com>
2018-01-28 13:32:16 +0400

    test conducted for read of word frequency list

--- tests/test_distractor_selection.py
+++ tests/test_distractor_selection.py
@@ -1,8 +1,11 @@
 import unittest
 from hamcrest import *
-
+from engine.domain.distractor_selection import read_file_ordered_by_frequency
+path = 'C:\\Users\\Nisali Kularatne\\Documents\\final year project\\resources\\wordlist_byalphabets'
 class TestDistractorSelection(unittest.TestCase):
+
     def test_read_file_by_frequency(self):
-        
-        assert_that(newword,any_of('a person who acts and gets things done', 'a theatrical performer'))
+        expected_res=('2186369', 'a', 'det')
+        res=next(read_file_ordered_by_frequency())
+        self.assertEqual(expected_res,res)
 
