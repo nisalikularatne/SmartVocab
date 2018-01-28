a14cac3f30b5c4de9a35f159e362811c1d1afd82
Nisali Kularatne <nisalikularatne@yahoo.com>
2018-01-28 15:32:55 +0400

    path name changed to travis

--- tests/test_distractor_selection.py
+++ tests/test_distractor_selection.py
@@ -1,7 +1,7 @@
 import unittest
 from hamcrest import *
 from engine.domain.distractor_selection import read_file_ordered_by_frequency
-path = 'C:\\Users\\Nisali Kularatne\\Documents\\final year project\\resources\\wordlist_byalphabets'
+path = 'C:\\Users\\Nisali Kularatne\\Documents\\f\\resources\\wordlist_byalphabets'
 class TestDistractorSelection(unittest.TestCase):
 
     def test_read_file_by_frequency(self):
