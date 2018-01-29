09543ed52b52e7a7d404827cce0b9524c76d8f60
Nisali Kularatne <nisalikularatne@yahoo.com>
2018-01-29 18:01:06 +0400

    extra sense class variable added to word

--- engine/domain/WordModel.py
+++ engine/domain/WordModel.py
@@ -8,9 +8,11 @@ class Word:
         self.difficulty = difficulty
         self.definitions = [Definition(sense) for sense in wn.synsets(self.word)]
         self.definition = random.choice(self.definitions)
+        self.senses = [Sense(sense, parent=self) for sense in wn.synsets(self.word)]
     def __str__(self):
         return "The Word is: {}".format(self.word)
-
+    def __iter__(self):
+        return self.senses.__iter__()
 
 
 class Definition:
@@ -24,10 +26,47 @@ class Definition:
     def __repr__(self):
         return "{}".format(self.definition)
 
+class Sense:
+   
+    def __init__(self, sense, parent):
+        # Parse the name to get word
+        splitted = sense.name().split('.')
+        if len(splitted) == 3:
+            self.sense_word = sense.name().split('.')[0]
+        else:
+            self.sense_word = ".".join(splitted[:-2])
+        self.parent_word = parent
+        self.sense = sense
+        self.id = int(sense.name().split('.')[-1])
+        self.pos = sense.pos()
+        self.wordnet_name = sense.name()
+        self.definition = sense.definition()
+
+
+
+    def __repr__(self):
+        return "<Sense {}>".format(self.wordnet_name)
+
+    def __lt__(self, other):
+        if self.parent_word_id == other.parent_word_id:
+            self.id < other.id
+        else:
+            self.parent_word_id < other.parent_word_id
+
+    def __hash__(self):
+        return hash(self.wordnet_name)
+
+    def __eq__(self, other):
+        return self.wordnet_name == other.wordnet_name
+
+
 
 
 if __name__ == "__main__":
 
     w = Word("actor")
     print(w.definition)
-    print(w) #print word
+    word = Word("rage")
+    for sense in word:
+        print(sense)
+
