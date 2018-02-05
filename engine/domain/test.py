958128ea3ba88e27df3d29c5508fbe6a76ac1aaa
Nisali Kularatne <nisalikularatne@yahoo.com>
2018-02-05 10:53:59 +0400

    Quiz Engine modified. More regress unit testing required as few bugs prevail 2

---
+++ engine/domain/test.py
@@ -0,0 +1,9 @@
+dict1 = {0: 33.422, 1: 39.2308, 2: 30.132}
+dict2 = {0: 42.2422, 1: 43.342, 2: 42.424}
+dict3 = {0: 13.422, 1: 9.2308, 2: 20.132}
+dicts = []
+dicts.append(dict1)
+dicts.append(dict2)
+dicts.append(dict3)
+if __name__ == "__main__":
+    print(dicts)
